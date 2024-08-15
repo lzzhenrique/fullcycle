d91593770199fac1d8adb36ea0790d72a8391219
Luiz Henrique <luiz.silva@agilize.com.br>
2024-08-15 13:16:16 -0300

    add desafio notification pattern em products

---
+++ fc-clean-architecture/src/infrastructure/api/presenters/customer.presenter.ts
@@ -0,0 +1,31 @@
+import { toXML } from "jstoxml";
+import {OutputListCustomerDto} from "../../../usecase/customer/list/list.customer.dto";
+import Customer from "../../../domain/customer/entity/customer";
+
+export default class CustomerPresenter {
+    static toXML(data: OutputListCustomerDto): string {
+        const xmlOptions = {
+            header: true,
+            indent: " ",
+            newLine: "\n",
+            allowEmpty: true,
+        };
+
+        const obj = {
+            customers: {
+                customer: data.customers.map((customer) => ({
+                    id: customer.id,
+                    name: customer.name,
+                    address: {
+                        street: customer.address.street,
+                        number: customer.address.number,
+                        zip: customer.address.zip,
+                        city: customer.address.city,
+                    },
+                })),
+            },
+        };
+
+        return toXML(obj, xmlOptions)
+    }
+}
